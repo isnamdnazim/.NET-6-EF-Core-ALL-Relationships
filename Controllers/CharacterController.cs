a63e7e46ff04a1c47b03bdefb943ccd56cc32372
Md Nazim Uddin <isnamdnazim@gmail.com>
2022-10-02 00:05:16 +0600

    Creating a Character is working fine

---
+++ Controllers/CharacterController.cs
@@ -0,0 +1,50 @@
+﻿using EFCoreRelationship.Data;
+using EFCoreRelationship.Dto;
+using EFCoreRelationship.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCoreRelationship.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CharacterController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public CharacterController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Character>>> Get(int userId)
+        {
+            var character = await _context.Characters.Where(c => c.UserId == userId)
+                .ToListAsync();
+            return Ok(character);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<Character>>> Create(CreateCharacterDto request)
+        {
+            var user = await _context.Users.FindAsync(request.UserId);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            var newCharacter = new Character
+            {
+                Name = request.Name,
+                RpgClass = request.RpgClass,
+                User = user
+            };
+
+            _context.Characters.Add(newCharacter);
+            await _context.SaveChangesAsync();
+            return await Get(newCharacter.UserId);
+        }
+    }
+}
--- Models/Character.cs
+++ Models/Character.cs
@@ -1,10 +1,13 @@
-﻿namespace EFCoreRelationship.Models
+﻿using System.Text.Json.Serialization;
+
+namespace EFCoreRelationship.Models
 {
     public class Character
     {
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
         public string RpgClass { get; set; } = "Knight";
+        [JsonIgnore]
         public User User { get; set; }
         public int UserId { get; set; }
     }
