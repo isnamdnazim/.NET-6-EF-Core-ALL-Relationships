36154bf8bb635e2d8e081403a25d4b831e1ba843
Md Nazim Uddin <isnamdnazim@gmail.com>
2022-10-01 22:24:16 +0600

    Added User And Character Model with Migration to SQL Server

---
+++ Data/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using EFCoreRelationship.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCoreRelationship.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Character> Characters { get; set; }
+    }
+}
---
+++ Models/Character.cs
@@ -0,0 +1,11 @@
+﻿namespace EFCoreRelationship.Models
+{
+    public class Character
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string RpgClass { get; set; } = "Knight";
+        public User User { get; set; }
+        public int UserId { get; set; }
+    }
+}
